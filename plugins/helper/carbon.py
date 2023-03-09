56d0bc2878d395bd9b7e85c0c0317b0846bab6b5
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2023-03-09 05:32:57 +0530

    Update carbon.py

--- plugins/helper/carbon.py
+++ plugins/helper/carbon.py
@@ -43,7 +43,7 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**ᴛʜɪs ᴘɪᴄ ɪs ɴɪᴄᴇ ᴏɴᴇ\nᴍᴀᴅᴇ ʙʏ @psycho network**",
+        caption="**ᴛʜɪs ᴘɪᴄ ɪs ɴɪᴄᴇ ᴏɴᴇ\nᴍᴀᴅᴇ ʙʏ @ᴍᴏᴠɪᴇꜱ ᴜɴʟᴏᴀᴅᴇᴅ**",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=S_GROUP)]]),                   
     )
     await m.delete()
