e3932586433b6a332740785604884132b61f33fd
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2023-03-09 05:42:02 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [🍁 ᴍᴜsɪᴄ ᴄʜᴀɴɴᴇʟ 🍁](https://t.me/myfavmusiccollection)**\n**ᴘᴏᴡᴇʀᴇᴅ ʙʏ -›› [ᴘsʏᴄʜᴏ ɴᴇᴛᴡᴏʀᴋ](https://t.me/psycho_association)**\n@PSYCHO-NETWORK**'
+        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› **\n**ᴘᴏᴡᴇʀᴇᴅ ʙʏ -›› [ᴍᴏᴠɪᴇꜱ ᴜɴʟᴏᴀᴅᴇᴅ](https://t.me/MOVIES_UNLOADED_backup)**\n@ᴍᴏᴠɪᴇꜱ ᴜɴʟᴏᴀᴅᴇᴅ**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
